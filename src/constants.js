51cea87659d9e9ce64d52114795647d223983c51
madmadi <mo@madmadi.me>
2019-03-02 19:52:10 +0330

    add design files

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const MAP_WIDTH = 10000;
 export const MAP_HEIGHT = 10000;
-export const MAP_BACKGROUND_COLOR_HEX = 0x019767;
+export const MAP_BACKGROUND_COLOR_HEX = 0x000;
 export const CELL_WIDHT_IN_PX = 50;
 export const CELL_HEIGHT_IN_PX = 50;
